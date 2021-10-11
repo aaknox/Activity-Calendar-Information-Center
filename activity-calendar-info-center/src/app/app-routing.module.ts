41e7c403bfdcdf65b6777a9cdb168c3adbe7bdd8
aaknox <azhya.knox@gmail.com>
2021-10-11 12:48:13 -0500

    finished routing for app

--- activity-calendar-info-center/src/app/app-routing.module.ts
+++ activity-calendar-info-center/src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { DocumentationComponent } from './components/documentation/documentation.component';
+import { DownloadPageComponent } from './components/download-page/download-page.component';
+import { GettingStartedComponent } from './components/getting-started/getting-started.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'getting-started',
+    component: GettingStartedComponent
+  },
+  {
+    path: 'docs',
+    component: DocumentationComponent
+  },
+  {
+    path: 'download',
+    component: DownloadPageComponent
   },
   {
     path: "",
--- activity-calendar-info-center/src/app/components/navbar/navbar.component.ts
+++ activity-calendar-info-center/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +10,7 @@ export class NavbarComponent implements OnInit {
 
   public navbarCollapsed = true;
   
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
