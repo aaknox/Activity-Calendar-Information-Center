18cdf986c540f586ccfd7e1f9ef4f82994a939d1
aaknox <azhya.knox@gmail.com>
2021-10-11 10:39:21 -0500

    added components and deps

--- activity-calendar-info-center/src/app/app.module.ts
+++ activity-calendar-info-center/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DocumentationComponent } from './components/documentation/documentation.component';
+import { AboutComponent } from './components/about/about.component';
+import { DownloadPageComponent } from './components/download-page/download-page.component';
+import { GettingStartedComponent } from './components/getting-started/getting-started.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    DocumentationComponent,
+    AboutComponent,
+    DownloadPageComponent,
+    GettingStartedComponent
   ],
   imports: [
     BrowserModule,
